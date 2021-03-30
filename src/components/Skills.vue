682917a2c1caae9e6c8a14324e7994e2b6b0b766
pfreemanc <pfreemanc@gmail.com>
2021-03-30 18:05:55 -0400

    finished skills section mostly

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -18,6 +18,13 @@
         </li>
       </ul>
     </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="svg">
+      <path
+        fill="#000000"
+        fill-opacity="1"
+        d="M0,128L80,154.7C160,181,320,235,480,218.7C640,203,800,117,960,106.7C1120,96,1280,160,1360,192L1440,224L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+      ></path>
+    </svg>
   </div>
 </template>
 
@@ -38,6 +45,11 @@ import db from "../../db.json";
 
 <style lang="scss">
 .skills {
+
+  svg {
+    transform: rotate(180deg);
+    margin-bottom: -5px;
+  }
 
   &__heading-container {
     text-align: center;

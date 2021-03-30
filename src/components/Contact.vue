4380ca9cca00a25219ad4987e598144ab82155c7
pfreemanc <pfreemanc@gmail.com>
2021-03-30 18:10:09 -0400

    initial commit

---
+++ src/components/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="contact">
+    <div class="contact--wrapper">
+      <div class="contact__heading-container">
+        <h2>Get In Touch</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss">
+
+</style>

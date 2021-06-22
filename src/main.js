7d28130abcaa63e2448f53aae8250288e454a56b
pfreemanc <pfreemanc@gmail.com>
2021-06-22 17:57:19 -0400

    Font-awesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faTwitter,
+  faLinkedinIn,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faTwitter, faLinkedinIn, faGithub, faEnvelope);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
